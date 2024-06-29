5628059d88e82ae5f2d02cea1b2029c366817e76
l-flaherty <lflaherty1@alum.babson.edu>
2024-06-29 18:24:08 -0400

    Loop through multiple pages and write function

--- ST558 Project 2 R Code.R
+++ ST558 Project 2 R Code.R
@@ -7,16 +7,23 @@ library("tidyverse")
 library("httr")
 library("jsonlite")
 
+
+
+
+
 #####2. Query APIs#####
-###2a. Set up base urls, enpoints, and keys###
+###2a. Set up base urls, endpoints, and keys###
 treasury_base_url="https://api.fiscaldata.treasury.gov/services/api/fiscal_service"
+
 treasury_endpoint="/v1/accounting/od/tips_cpi_data_summary"
 treasury_endpoint_FX="/v1/accounting/od/rates_of_exchange"
+
 
 
 ###2b. Set up url###
 treasury_url=paste0(treasury_base_url, treasury_endpoint)
 treasury_url1=paste0(treasury_base_url, treasury_endpoint_FX)
+
 
 
 ###2c. Query API###
@@ -27,11 +34,56 @@ str(api_return, max.level=1)    #glimpse of what the API returns#
 plaintext=fromJSON(rawToChar(api_return$content))
 str(plaintext, max.level=1)    #plaintext$data seems to be format of most of them#
 
+plaintext$data                 #record date not recent#
+
+
+
+###2d. Getting correct number of pages###
 #note that by default, API call only returns 100 observations on 1 page#
 #https://fiscaldata.treasury.gov/datasets/treasury-reporting-rates-exchange/treasury-reporting-rates-of-exchange...#
 #...gives information about pagination and other parts of the call#
+#format added on to endpoint is e.g. "?page[number]=1&page[size]=3"#
 #Can do plaintext$meta and see `total-count` and `total-pages` to expedite call#
 #e.g. plaintext$meta[["total-pages"]] or plaintext$meta[["total-count"]]#
+
+plaintext$meta[["total-count"]]     #17368#
+plaintext$meta[["total-pages"]]     #174#
+
+treasury_url=paste0(
+  treasury_base_url,
+  treasury_endpoint_FX,
+  "?page[number]=", 1,
+  "&page[size]=", 17368
+)
+
+api_return1=GET(treasury_url) 
+plaintext=fromJSON(rawToChar(api_return1$content))
+str(plaintext, max.level=1)        #error message: max per page is 10000#
+
+
+
+###2e. Loop through pages###
+page_size=1000
+page_number=round(plaintext$meta[["total-count"]]/page_size)+1
+
+treasury_df=data.frame()
+for (i in 1:page_number) {
+  
+  treasury_url=paste0(
+    treasury_base_url,
+    treasury_endpoint_FX,
+    "?page[number]=", i,
+    "&page[size]=", 1000)
+  
+  api=GET(treasury_url)
+  plain=fromJSON(rawToChar(api$content))
+  a=plain$data
+  
+  treasury_df=rbind(treasury_df, a)
+}
+
+unique(treasury_df$record_date)            #looks good#
+
 
 
 ###2c. Create UDF To Make Easier###
@@ -50,10 +102,34 @@ treasury=function(data_type) {
   
   api_return=GET(paste0(base_url, endpoint))
   plaintext=fromJSON(rawToChar(api_return$content))
-  return(plaintext$data)
+  
+  page_size=1000
+  page_number=round(plaintext$meta[["total-count"]]/page_size)+1
+  
+  df=data.frame()
+  
+  for (i in 1:page_number) {
+    
+    url=paste0(
+      base_url,
+      endpoint,
+      "?page[number]=", i,
+      "&page[size]=", 1000)
+    
+    api=GET(url)
+    plain=fromJSON(rawToChar(api$content))
+    a=plain$data
+    
+    df=rbind(df, a)
+  }
+  
+  return(as_tibble(df))
 }
 
              
+
+
+
 
 
 #########scrap#################
